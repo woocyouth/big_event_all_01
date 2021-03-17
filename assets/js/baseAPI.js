56d9e884f70b7f8c97c112f1bb36d3e673366049
woocyouth <15107510579@163.com>
2021-03-17 15:43:42 +0800

    user-01

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,7 +1,7 @@
 let baseURL = "http://api-breakingnews-web.itheima.net";
 
 $.ajaxPrefilter(function (options) {
-    console.log(options);
+    // console.log(options);
     options.url = baseURL + options.url;
 
     if (options.url.indexOf("/my/") != -1) {
