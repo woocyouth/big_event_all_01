33c5bd883bb2cfd05e81cb1060ae2af0c61a9d06
woocyouth <15107510579@163.com>
2021-03-17 14:44:58 +0800

    index-01

---
+++ assets/js/index.js
@@ -0,0 +1,33 @@
+$(function () {
+    let layer = layui.layer;
+    let form = layui.form;
+
+    initIndex();
+
+    function initIndex() {
+        $.ajax({
+            url: '/my/userinfo',
+            dataType: 'json',
+            success: (res) => {
+                console.log(res);
+                if (res.status != 0) {
+                    return layer.msg(res.message, {
+                        icon: 5
+                    });
+                }
+
+                let name = res.data.nickname || res.data.username;
+                $(".welcome").text(name);
+                if (res.data.user_pic) {
+                    $(".layui-nav-img").attr("src", res.data.user_pic);
+                    $(".userinfo").hide();
+                } else {
+                    $(".userinfo").hide();
+                    $(".welcome").text(name[0]);
+                }
+            }
+        })
+    }
+
+
+})
